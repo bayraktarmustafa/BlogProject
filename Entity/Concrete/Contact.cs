1318f41964893a0fa6bf405b49cb22160593eac9
Mustafa Bayraktar <m.bayraktar01@hotmail.com>
2023-03-01 15:52:16 +0300

    Add project files.

---
+++ Entity/Concrete/Contact.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Concrete
+{
+    public class Contact
+    {
+        public int ContactID { get; set; }
+
+        public string ContactUserName { get; set; }
+
+        public string ContactMail { get; set; }
+
+        public string ContactSubject { get; set; }
+        public string ContactMessage { get; set; }
+
+        public DateTime ContactDate { get; set; }
+
+        public bool ContactStatus { get; set; }
+
+
+
+    }
+}
