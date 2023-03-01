78af1b7532f4804e86b76cd84112e8df64c2ff24
Mustafa Bayraktar <m.bayraktar01@hotmail.com>
2023-03-01 16:10:53 +0300

    EntityFramework update

---
+++ Data Access/Concrete/Context/Context.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Data_Access.Concrete.Context
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"server=MUSTAFA;database=BlogDB;integrated security=true;");
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+
+    }
+}
--- Entity/Concrete/Contact.cs
+++ Entity/Concrete/Contact.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Security.AccessControl;
 using System.Text;
@@ -9,6 +10,7 @@ namespace Entity.Concrete
 {
     public class Contact
     {
+        [Key]
         public int ContactID { get; set; }
 
         public string ContactUserName { get; set; }
